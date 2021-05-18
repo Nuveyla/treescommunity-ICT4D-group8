17eb7c127877b46eaf20a85f66cbaafb67f48b3e
Melda Malkoc <37611243+Nuveyla@users.noreply.github.com>
2021-05-18 18:07:20 +0200

    Add files via upload

--- php.php
+++ php.php
@@ -1,33 +1,7 @@
 <?php
-
-// PDO connection
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
-
-// handler to query the database
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
-
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
-
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
-});
+$hostname = "ec2-54-154-101-45.eu-west-1.compute.amazonaws.com";
+$username = "pepxcmjxinxtjg";
+$pass = "bdf401aa894f876599e91bcadcced264662a879f9554274134ac4f9c0e557505";
+$dbname = "d9786qfc0a7hrr";
+$db_conn = pg_connect("host = $hostname dbname = $dbname user = $username password = $pass");
 ?>
